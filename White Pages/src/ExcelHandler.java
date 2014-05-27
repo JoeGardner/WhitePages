df5db9bdf72bb98ea27b4aa469d59ec2678da6e7
JoeGardner <j.oewngardner@gmail.com>
2014-05-27 06:45:04 -0500

    First Commit of 2014

---
+++ White Pages/src/ExcelHandler.java
@@ -0,0 +1,12 @@
+
+public class ExcelHandler {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+
+	}
+
+}
---
+++ White Pages/src/WebHandler.java
@@ -0,0 +1,12 @@
+
+public class WebHandler {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		
+	}
+
+}
